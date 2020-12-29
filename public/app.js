faba5630bb75431ca213afba081bf3585bd31b2f
古田陸太 <rikuta@rikutaMBP.local>
2020-12-30 04:41:46 +0900

    2.4.2

--- public/app.js
+++ public/app.js
@@ -2,6 +2,18 @@
 var learnjs = {};
 
 learnjs.showView = function(hash){
-	var problemView = $('<div class="problem-view">').text('Coming Soon...');
-	$('.view-container').empty().append(problemView);
+	var routes = {
+		'#problem':learnjs.problemView
+	};
+	var hashParts = hash.split("-");
+	var viewFn=routes[hashParts[0]];
+	if(viewFn){
+		$('.view-container').empty().append(viewFn(hashParts[1]));
+	}
+}
+
+learnjs.problemView = function(problemNumber){
+	var title = "Problem #" + problemNumber + " Coming Soon..."
+	var problemView = $("<div class=\"problem-view\">").text(title);
+	return problemView
 }
